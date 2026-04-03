ce30f85b6a0bb0bfc8bb00f95265552bf9d55f4c
zhangxianyi <zhangxianyi@cestc.cn>
2026-04-03 17:04:16 +0800

    http proxy for domainList

--- src/worker/client.go
+++ src/worker/client.go
@@ -3,6 +3,8 @@ package worker
 import (
 	"net"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -32,13 +34,30 @@ func (c *Client) getConditional(url, lastModified, etag string) (*http.Response,
 
 var client *Client
 
+// 固化代理配置 - 请根据需要修改这些值
+const proxyURL = "http://127.0.0.1:17890"          // 替换为你的代理地址
+var domainList = []string{"rsshub.rssforever.com"} // 替换为需要使用代理的域名列表
+
+// 代理函数：匹配域名列表时使用指定代理，否则使用环境变量代理
+func getProxyFunc(proxyURL string, domains []string) func(*http.Request) (*url.URL, error) {
+	return func(req *http.Request) (*url.URL, error) {
+		for _, domain := range domains {
+			if strings.Contains(req.URL.Host, domain) {
+				return url.Parse(proxyURL)
+			}
+		}
+		// 不匹配时使用环境变量代理
+		return http.ProxyFromEnvironment(req)
+	}
+}
+
 func SetVersion(num string) {
 	client.userAgent = "Yarr/" + num
 }
 
 func init() {
 	transport := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
+		Proxy: getProxyFunc(proxyURL, domainList),
 		DialContext: (&net.Dialer{
 			Timeout: 10 * time.Second,
 		}).DialContext,
